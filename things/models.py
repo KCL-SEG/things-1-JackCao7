62e48ff4110237c02b5460a33462367bfe37b24e
“JACK <jack.cao@kcl.ac.uk>
2023-12-06 10:26:32 +0000

    Added migrations

--- things/models.py
+++ things/models.py
@@ -5,5 +5,5 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Thing(models.Model):
     name = models.CharField(blank=False, unique=True, max_length=30)
-    description = models.TextField(max_length=120)
+    description = models.CharField(blank=True, unique=False, max_length=120)
     quantity = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
